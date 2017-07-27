9944c1a3a0bc5aef98d4d6d97a354969d2ae7782
rickmurdock <rick.murdock@live.com>
2017-07-27 18:44:02 -0500

    Initial project files.

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class componentName extends Component {
+  render() {
+    return (
+      <div>
+        <h1>BaseLayout</h1>
+      </div>
+    );
+  }
+}
+
+export default componentName;
